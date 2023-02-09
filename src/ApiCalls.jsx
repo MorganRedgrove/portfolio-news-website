f0bae942fdf1761bd8f369b3e3468c643c684ccf
MorganRedgrove <morganekelunddavies@gmail.com>
2023-02-09 14:42:12 +0000

    sorting implemented

--- src/ApiCalls.jsx
+++ src/ApiCalls.jsx
@@ -4,10 +4,27 @@ const apiCaller = axios.create({
     baseURL: "https://nc-news-xe9m.onrender.com/api",
 })
 
-export const getArticles = () => {
-    return apiCaller.get("/articles")
+export const getArticles = (sort_by, order) => {
+    let endpoint = "/articles"
+
+    if (sort_by || order) {
+        endpoint += "?"
+    }
+
+    if (sort_by && !order) {
+        endpoint += `sort_by=${sort_by}`
+    } else if (order && !sort_by) {
+        endpoint += `order=${order}`
+    } else if (sort_by && order) {
+        endpoint += `sort_by=${sort_by}&order=${order}`
+    }
+
+    return apiCaller.get(endpoint)
         .then((response) => {
             return response.data.articles
+        })
+        .catch((err) => {
+            console.log(err)
         })
 }
 
--- src/Articles.jsx
+++ src/Articles.jsx
@@ -1,15 +1,43 @@
-import { useContext } from "react"
+import { useState, useEffect, useContext } from "react"
 
 import { Banner } from "./Banner"
 import { Footer } from "./Footer"
 import { ArticleCard } from "./ArticleCard"
 import { Loading } from "./Loading"
 
+import { getArticles } from "./ApiCalls"
+
 import { LoadingContext } from "./contexts/contexts"
 
 
-export const Articles = ({ articles }) => {
+export const Articles = () => {
     const { isLoading, setIsLoading } = useContext(LoadingContext)
+
+    const [articles, setArticles] = useState([])
+    const [sort_by, setSortBy] = useState("")
+    const [order, setOrder] = useState("")
+
+    const articleKeys = ["title", "topic", "author", "created_at"]
+
+    const changeHandler = (event, setFunc) => {
+        setFunc(event.target.value)
+    }
+
+    useEffect(() => {
+        setIsLoading(true)
+        getArticles(sort_by, order)
+            .then((articles) => {
+                setArticles(articles)
+                setIsLoading(false)
+            })
+    }, [])
+
+    useEffect(() => {
+        getArticles(sort_by, order)
+            .then((articles) => {
+                setArticles(articles)
+            })
+    }, [sort_by, order])
 
     return (
         <div>
@@ -17,6 +45,25 @@ export const Articles = ({ articles }) => {
 
             <div id="content">
                 <h1>Articles</h1>
+
+                <div className="articles-dropdown">
+                    <label htmlFor="articles-sort">Sort by:</label>
+                    <select id="articles-sort" onChange={(event) => { changeHandler(event, setSortBy) }}>
+                        <option value="">Select</option>
+                        {articleKeys.map((key) => {
+                            return (
+                                <option key={key} value={key}>{key}</option>
+                            )
+                        })}
+                    </select>
+
+                    <label htmlFor="articles-order">Order:</label>
+                    <select id="articles-order" onChange={(event) => { changeHandler(event, setOrder) }}>
+                        <option value="DESC">Descending</option>
+                        <option value="ASC">Ascending</option>
+
+                    </select>
+                </div>
 
                 {isLoading ? <Loading /> : null}
 
